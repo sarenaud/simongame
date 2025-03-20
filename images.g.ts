dfbbda6c31e4f76b23230d3ccd51c1618a027f93
sarenaud <32renaud.simon@gmail.com>
2025-03-20 12:57:51 -0400

    Initial files for MakeCode project

---
+++ images.g.ts
@@ -0,0 +1,209 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image1":
+            case "cat_maybe":return img`
+. . . . . . . . . . . . . . 
+e e e . . . . e e e . . . . 
+c d d c . . c d d c . . . . 
+c b d d f f d d b c . . . . 
+c 3 b d d b d b 3 c . . . . 
+f b 3 d d d d 3 b f . . . . 
+e d d d d d d d d e . . . . 
+e d f d d d d f d e . b f b 
+f d d f d d f d d f . f d f 
+f b d d b b d d 2 b f f d f 
+. f 2 2 2 2 2 2 d b b d b f 
+. f d d d d d d d f f f f . 
+. . f d b d f d f . . . . . 
+. . . f f f f f f . . . . . 
+`;
+            case "image3":
+            case "mom":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . 6 6 6 6 . . 
+. . . . . . . . . . 1 1 6 6 6 . 
+. . . . . . . . . 1 1 1 1 6 6 . 
+. . . . . . . . . . 1 1 6 6 6 . 
+. . . . . . . . . . 1 1 6 3 3 . 
+. . . . . . . . . . . 1 6 5 . . 
+. . . . . . 8 8 8 8 8 b 5 . . . 
+. . . . f 8 b b b b b 8 5 . . . 
+. . . f f 3 3 3 3 3 3 3 . . . . 
+. . f f f 3 3 3 3 3 3 3 . . . . 
+. . f 8 8 5 . 5 . a a a . . . . 
+. 1 1 8 . 5 . 5 a 5 . 5 a . . . 
+. 8 8 . . 5 5 5 a 5 a 5 a . . . 
+. . . . a a . . a . . . a . . . 
+. . . . a a . . . a a a . . . . 
+`;
+            case "image4":
+            case "tunnel worm":return img`
+. . . . . . . . . . . . . . . . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . . . . . . . . . . . . . . . 
+. . f f f f . . . f f . . . f f . . . f f . . . f f f f . . 
+. f f f f f f . f f f f . f f f f . f f f f . f f f f f f . 
+f f a f f f f f f f f f f f f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f 
+. f f f f f f . f f f f . f f f f . f f f f . f f f f f f . 
+. . a . a . a . a . . a . a . . a . a . . a . a . a . a . . 
+`;
+            case "image5":
+            case "tunnel worm2":return img`
+. . . . . . . . . . . . . . . . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . . . . . . . . . . . . . . . 
+. . 6 6 6 6 . . . 6 6 . . . 6 6 . . . 6 6 . . . 6 6 6 6 . . 
+. 6 6 6 6 6 6 . 6 6 6 6 . 6 6 6 6 . 6 6 6 6 . 6 6 6 6 6 6 . 
+6 6 f 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 
+6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 
+. 6 6 6 6 6 6 . 6 6 6 6 . 6 6 6 6 . 6 6 6 6 . 6 6 6 6 6 6 . 
+. . a . a . a . a . . a . a . . a . a . . a . a . a . a . . 
+`;
+            case "image7":
+            case "tall monster":return img`
+....333333333333333.....
+...33399333333399333....
+..3339999333339999333...
+..33399993aaa39999333...
+..33339933aaa33993333...
+...33333333333333333....
+....333333333333333.....
+.....a..a...a....a......
+....aa..a...aa...aa.....
+.....a.aa..aa.....aa....
+...aaa..aa..a......aaa..
+..aa....a...aa......a...
+..a....aa...a......aa...
+..aa....a...aa......a...
+.aa.....aa..aa.....aa...
+aa.....aa....a.....aa...
+.a......aa..aa.....a....
+.a......a....aa...aa....
+.aa....aa....a.....a....
+..aa....aa..aaa...aa....
+.aa.....a...a....aa.....
+..a.....aa.aa.....a.....
+.aa....aa...aa...aa.....
+..a.....a...a.....a.....
+`;
+            case "image9":
+            case "kind monster":return img`
+..............................
+..............................
+..66666666666666666666666666..
+1.666699f996666666699f996666.1
+1166699dfd9966666699dfd9966611
+116699ddfdd99666699ddfdd996611
+1.66699dfd9966666699dfd99666.1
+..666699f9966aaa66699f996666..
+..66666666666aaa666666666666..
+..66666666666666666666666666..
+....33..................33....
+.....33................33.....
+.....33................33.....
+.....33................33.....
+.....3..................3.....
+...333..................333...
+`;
+            case "image8":
+            case "bug eye monster":return img`
+. . . . . 2 2 . . . . . . . . . 
+. . 2 2 2 2 2 2 2 2 . . . . . . 
+. 9 2 2 2 a a 2 2 2 9 . . . . . 
+. . . . 2 2 2 2 . . . . . . . . 
+. . . . . 6 6 . . . . . . . . . 
+. 2 2 2 2 6 6 2 2 2 2 . . . . . 
+. 2 . . . 6 6 . . . 2 . . . . . 
+. 2 . . 6 6 6 6 . . 2 . . . . . 
+. 2 . . 6 6 6 6 . . 2 . . . . . 
+. 2 . . 6 6 6 6 . . 2 . . . . . 
+. 2 . . 6 6 6 6 . . 2 . . . . . 
+. . . 2 6 6 6 6 2 . . . . . . . 
+. . . 2 . 6 6 . 2 . . . . . . . 
+. . . 2 . 6 6 . 2 . . . . . . . 
+. . . 2 . . . . 2 . . . . . . . 
+. . . 2 . . . . 2 . . . . . . . 
+`;
+            case "image2":
+            case "character":return img`
+. . . . . . 3 3 3 . . . . . . . 
+. . . . . . 3 1 1 . . . . . . . 
+. . . . . . 3 1 1 1 . . . . . . 
+. . . . . . 1 1 1 . . . . . . . 
+. . . . . . . 1 1 . . . . . . . 
+. . . . . . f f f . . . . . . . 
+. . . . . f f f f f . . . . . . 
+. . . . . f a f f f . . . . . . 
+. . . . . f a f f f . . . . . . 
+. . . . . f a 1 f f 1 . . . . . 
+. . . . . f f f f f . . . . . . 
+. . . . . b b b b b . . . . . . 
+. . . . . b b . b b . . . . . . 
+. . . . . b b . b b . . . . . . 
+. . . . . b b . b b . . . . . . 
+. . . . f f f . f f f . . . . . 
+`;
+            case "image6":
+            case "Cute monster":return img`
+. . . . . . . . 6 . . . . . . . 
+. . . . . . . . 6 . . . . . . . 
+. . . . . . 6 6 6 6 6 . . . . . 
+. . . . 6 6 6 6 6 6 6 6 6 . . . 
+. . . . 6 6 6 6 6 6 6 6 6 6 . . 
+. . . 6 6 6 9 6 6 6 6 6 6 6 6 . 
+. . . 6 6 9 9 9 6 6 9 9 9 9 6 . 
+. . 6 6 6 9 9 9 6 6 6 9 9 6 6 . 
+. . 6 6 6 6 9 6 6 6 6 6 6 6 6 . 
+a a 6 6 6 6 6 a a a 6 6 6 6 6 a 
+a . 6 6 6 6 a f f a 6 6 6 6 . a 
+a . . 6 a a f f f f a 6 6 6 . a 
+a a . 6 6 6 a a a a 6 6 6 . a a 
+a a . . 6 6 6 6 6 6 6 6 . . a a 
+a a . . . . 6 6 6 6 . . . . a a 
+a a . . . . . . . . . . . . a a 
+`;
+            case "image10":
+            case "doorSprite":return img`
+d d d d d d d d d d d d d d d d 
+d d d d d d 3 3 3 3 d d d d d d 
+d d d d d 3 4 4 4 4 3 d d d d d 
+d d d d 3 3 4 f f 4 3 3 d d d d 
+d d d d 3 3 4 4 4 4 3 3 d d d d 
+d d d 3 3 3 3 3 3 3 3 3 3 d d d 
+d d d 3 4 4 4 3 3 4 4 4 3 d d d 
+d d d 3 4 f 4 3 3 4 f 4 3 d d d 
+d d d 3 4 4 4 3 3 4 4 4 3 d d d 
+d d d 3 4 f 4 3 3 4 f 4 3 d d d 
+d d d 3 4 f 4 3 3 4 f 4 3 d d d 
+d d d 3 4 f 4 3 3 4 f 4 3 d d d 
+d d d 3 4 f 4 3 3 4 f 4 3 d d d 
+d d d 3 4 4 4 3 3 4 4 4 3 d d d 
+d d d 3 4 f 4 3 3 4 f 4 3 d d d 
+d d d 3 4 4 4 3 3 4 4 4 3 d d d 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("song", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
---
+++ main.ts
@@ -0,0 +1,376 @@
+namespace SpriteKind {
+    export const npc = SpriteKind.create()
+    export const npc1 = SpriteKind.create()
+    export const npc2 = SpriteKind.create()
+}
+scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile77`, function (sprite, location) {
+    if (level == 7) {
+        tiles.setCurrentTilemap(maps[0])
+        level = 0
+        controller.moveSprite(mySprite, 100, 0)
+        mySprite.setScale(1, ScaleAnchor.Middle)
+        tiles.placeOnTile(mySprite, tiles.getTileLocation(19, 29))
+        mySprite.ay = 1000
+    }
+})
+sprites.onOverlap(SpriteKind.Player, SpriteKind.npc, function (sprite, otherSprite) {
+    if (otherSprite == mysprite2) {
+        timer.throttle("action", 5000, function () {
+            mysprite2.sayText("Hello", 5000, false)
+        })
+    } else if (otherSprite == mySprite3) {
+        timer.throttle("action", 10000, function () {
+            mySprite3.sayText("Watch out-", 2000, false)
+            timer.after(2000, function () {
+                mySprite3.sayText("I wouldn't go down there. . .", 4000, false)
+            })
+            timer.after(4000, function () {
+                mySprite3.sayText("unless you're sure you're ready!", 4000, false)
+            })
+        })
+        if (otherSprite == doorNPC) {
+            timer.throttle("action", 1000, function () {
+                doorNPC.sayText("bruh", 1000, false)
+            })
+        }
+    }
+})
+scene.onOverlapTile(SpriteKind.Player, assets.tile`Door for house 3`, function (sprite, location) {
+    if (level == 4) {
+        tiles.setCurrentTilemap(maps[0])
+        mySprite.setPosition(690, 120)
+        level = 0
+        controller.moveSprite(mySprite, 100, 0)
+        mySprite.setScale(1, ScaleAnchor.Middle)
+        mysprite2.setPosition(1050, 345)
+        mySprite.ay = 1000
+    }
+})
+scene.onOverlapTile(SpriteKind.Player, assets.tile`Dirt3`, function (sprite, location) {
+    if (level == 1) {
+        tiles.setCurrentTilemap(maps[0])
+        mySprite.setPosition(1145, 345)
+        level = 0
+        mysprite2 = sprites.create(assets.image`bug eye monster`, SpriteKind.npc)
+        controller.moveSprite(mySprite, 100, 100)
+        mySprite.ay = 1000
+        mysprite2.setPosition(1050, 345)
+        mySprite.setScale(1, ScaleAnchor.Middle)
+        mySprite3 = sprites.create(assets.image`bug eye monster`, SpriteKind.npc)
+        tiles.placeOnTile(mySprite3, tiles.getTileLocation(20, 31))
+        Mysprite_5 = sprites.create(assets.image`bug eye monster`, SpriteKind.npc)
+        tiles.placeOnTile(Mysprite_5, tiles.getTileLocation(34, 26))
+        MySprite6 = sprites.create(assets.image`bug eye monster`, SpriteKind.npc)
+        tiles.placeOnTile(MySprite6, tiles.getTileLocation(3, 21))
+        MySprite_7 = sprites.create(assets.image`bug eye monster`, SpriteKind.npc)
+        tiles.placeOnTile(MySprite_7, tiles.getTileLocation(42, 18))
+        doorNPC = sprites.create(assets.image`doorSprite`, SpriteKind.npc)
+        tiles.placeOnTile(doorNPC, tiles.getTileLocation(77, 55))
+    }
+})
+scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile24`, function (sprite, location) {
+    if (controller.B.isPressed()) {
+        if (level == 0) {
+            tiles.setCurrentTilemap(maps[4])
+            mySprite.setPosition(175, 40)
+            level = 4
+            controller.moveSprite(mySprite, 100, 100)
+            mySprite.ay = 0
+            mySprite.setScale(2, ScaleAnchor.Middle)
+        }
+    }
+})
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    if (mySprite.isHittingTile(CollisionDirection.Bottom)) {
+        mySprite.vy = -200
+    }
+})
+scene.onOverlapTile(SpriteKind.Player, assets.tile`LightTile`, function (sprite, location) {
+    multilights.removeLightSource(sprite)
+    multilights.addLightSource(
+    sprite,
+    30,
+    10,
+    2.5
+    )
+    mySprite.sayText("on", 2000, false)
+})
+scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile4`, function (sprite, location) {
+    if (controller.up.isPressed()) {
+        mySprite.vy = -75
+    }
+    if (controller.down.isPressed()) {
+        mySprite.vy = 75
+    }
+})
+scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile73`, function (sprite, location) {
+    if (level == 5) {
+        tiles.setCurrentTilemap(maps[6])
+        level = 6
+        mySprite.setScale(2, ScaleAnchor.Top)
+        tiles.placeOnTile(mySprite, tiles.getTileLocation(5, 13))
+    }
+})
+scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile25`, function (sprite, location) {
+    if (controller.B.isPressed()) {
+        if (level == 0) {
+            tiles.setCurrentTilemap(maps[5])
+            mySprite.setPosition(175, 40)
+            level = 5
+            controller.moveSprite(mySprite, 100, 100)
+            mySprite.ay = 0
+            mySprite.setScale(2, ScaleAnchor.Middle)
+            mysprite_4 = sprites.create(assets.image`Cute monster`, SpriteKind.npc1)
+            tiles.placeOnTile(mysprite_4, tiles.getTileLocation(11, 7))
+        }
+    }
+})
+scene.onOverlapTile(SpriteKind.Player, assets.tile`Door for house 2`, function (sprite, location) {
+    if (level == 3) {
+        tiles.setCurrentTilemap(maps[0])
+        mySprite.setPosition(685, 375)
+        level = 0
+        controller.moveSprite(mySprite, 100, 0)
+        mySprite.setScale(1, ScaleAnchor.Middle)
+        mysprite2.setPosition(1050, 345)
+        mySprite.ay = 1000
+    }
+})
+scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile29`, function (sprite, location) {
+    if (controller.B.isPressed()) {
+        if (level == 0) {
+            tiles.setCurrentTilemap(maps[14])
+            mySprite.setPosition(175, 40)
+            level = 14
+            controller.moveSprite(mySprite, 100, 100)
+            mySprite.ay = 0
+            mySprite.setScale(2, ScaleAnchor.Middle)
+        }
+    }
+})
+scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile74`, function (sprite, location) {
+    if (level == 7) {
+        tiles.setCurrentTilemap(maps[8])
+        level = 8
+        tiles.placeOnTile(mySprite, tiles.getTileLocation(12, 3))
+    } else if (level == 8) {
+        tiles.setCurrentTilemap(maps[7])
+        level = 7
+        tiles.placeOnTile(mySprite, tiles.getTileLocation(12, 3))
+    }
+})
+scene.onOverlapTile(SpriteKind.Player, assets.tile`Door for house 0`, function (sprite, location) {
+    if (level == 5) {
+        tiles.setCurrentTilemap(maps[0])
+        mySprite.setPosition(1080, 345)
+        level = 0
+        controller.moveSprite(mySprite, 100, 0)
+        mysprite2.setPosition(1050, 345)
+        mySprite.ay = 1000
+        mySprite.setScale(1, ScaleAnchor.Middle)
+    }
+})
+scene.onOverlapTile(SpriteKind.Player, assets.tile`TunnelTileDown`, function (sprite, location) {
+    if (controller.up.isPressed()) {
+        mySprite.vy = -75
+    }
+    if (controller.down.isPressed()) {
+        mySprite.vy = 75
+    }
+})
+sprites.onOverlap(SpriteKind.Player, SpriteKind.npc1, function (sprite, otherSprite) {
+    if (otherSprite == mysprite_4) {
+        timer.throttle("action", 5000, function () {
+            mysprite_4.sayText("I'm a cute little guy!", 5000, false)
+        })
+    }
+})
+scene.onOverlapTile(SpriteKind.Player, assets.tile`LightTileoff`, function (sprite, location) {
+    multilights.removeLightSource(sprite)
+    multilights.addLightSource(
+    sprite,
+    2,
+    13,
+    2.5
+    )
+    mySprite.sayText("off", 2000, false)
+})
+scene.onOverlapTile(SpriteKind.Player, assets.tile`Door for house 1`, function (sprite, location) {
+    if (level == 2) {
+        tiles.setCurrentTilemap(maps[0])
+        level = 0
+        controller.moveSprite(mySprite, 100, 0)
+        mySprite.setScale(1, ScaleAnchor.Middle)
+        tiles.placeOnTile(mySprite, tiles.getTileLocation(35, 7))
+        mySprite.ay = 1000
+    }
+})
+scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile75`, function (sprite, location) {
+    if (level == 6) {
+        tiles.setCurrentTilemap(maps[5])
+        level = 5
+        mySprite.setScale(2, ScaleAnchor.Top)
+        tiles.placeOnTile(mySprite, tiles.getTileLocation(5, 13))
+    }
+})
+scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile27`, function (sprite, location) {
+    if (controller.B.isPressed()) {
+        if (level == 0) {
+            tiles.setCurrentTilemap(maps[1])
+            mySprite.setPosition(85, 160)
+            level = 1
+            controller.moveSprite(mySprite, 100, 100)
+            mySprite.ay = 0
+            mySprite.setScale(2, ScaleAnchor.Middle)
+        }
+    }
+})
+scene.onOverlapTile(SpriteKind.Player, assets.tile`storedoor1`, function (sprite, location) {
+    if (controller.B.isPressed()) {
+        if (level == 0) {
+            tiles.setCurrentTilemap(maps[7])
+            mySprite.setPosition(140, 350)
+            level = 7
+            controller.moveSprite(mySprite, 100, 100)
+            mySprite.ay = 0
+            mySprite.setScale(2, ScaleAnchor.Middle)
+        }
+    }
+})
+sprites.onOverlap(SpriteKind.Player, SpriteKind.npc2, function (sprite, otherSprite) {
+    if (otherSprite == NPC8) {
+        timer.throttle("action", 2000, function () {
+            NPC8.sayText("Go back, now!", 2000, false)
+        })
+    }
+    if (otherSprite == NPC9) {
+        timer.throttle("action", 4000, function () {
+            NPC9.sayText("Why are you still trying?", 2000, false)
+            timer.after(2000, function () {
+                NPC9.sayText("You're lost, idiot!", 2000, false)
+            })
+        })
+    }
+})
+scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile69`, function (sprite, location) {
+    if (level == 14) {
+        tiles.setCurrentTilemap(maps[0])
+        level = 0
+        controller.moveSprite(mySprite, 100, 0)
+        mySprite.setScale(1, ScaleAnchor.Middle)
+        tiles.placeOnTile(mySprite, tiles.getTileLocation(23, 29))
+        mySprite.ay = 1000
+    }
+})
+scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile23`, function (sprite, location) {
+    if (controller.B.isPressed()) {
+        if (level == 0) {
+            tiles.setCurrentTilemap(maps[2])
+            mySprite.setPosition(175, 40)
+            level = 2
+            controller.moveSprite(mySprite, 100, 100)
+            mySprite.ay = 0
+            mySprite.setScale(2, ScaleAnchor.Middle)
+        }
+    }
+})
+scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile11`, function (sprite, location) {
+    if (controller.up.isPressed()) {
+        mySprite.vy = -75
+    }
+    if (controller.down.isPressed()) {
+        mySprite.vy = 75
+    }
+})
+scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile30`, function (sprite, location) {
+    if (controller.B.isPressed()) {
+        if (level == 0) {
+            tiles.setCurrentTilemap(maps[3])
+            mySprite.setPosition(175, 40)
+            level = 3
+            controller.moveSprite(mySprite, 100, 100)
+            mySprite.ay = 0
+            mySprite.setScale(2, ScaleAnchor.Middle)
+        }
+    }
+})
+scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile26`, function (sprite, location) {
+    if (controller.B.isPressed()) {
+        if (level == 0) {
+            tiles.setCurrentTilemap(maps[10])
+            level = 10
+            tiles.placeOnTile(mySprite, tiles.getTileLocation(13, 22))
+            controller.moveSprite(mySprite, 100, 100)
+            mySprite.ay = 0
+            multilights.removeLightSource(mySprite)
+            multilights.addLightSource(
+            mySprite,
+            2,
+            13,
+            2.5
+            )
+        }
+        NPC8 = sprites.create(assets.image`Cute monster`, SpriteKind.npc2)
+        tiles.placeOnTile(NPC8, tiles.getTileLocation(5, 18))
+        NPC9 = sprites.create(assets.image`Cute monster`, SpriteKind.npc2)
+        tiles.placeOnTile(NPC9, tiles.getTileLocation(11, 22))
+    }
+})
+let NPC9: Sprite = null
+let NPC8: Sprite = null
+let mysprite_4: Sprite = null
+let MySprite_7: Sprite = null
+let MySprite6: Sprite = null
+let Mysprite_5: Sprite = null
+let doorNPC: Sprite = null
+let mySprite3: Sprite = null
+let mysprite2: Sprite = null
+let mySprite: Sprite = null
+let level = 0
+let maps: tiles.TileMapData[] = []
+maps = [
+tilemap`overworld`,
+tilemap`Character house`,
+tilemap`House1`,
+tilemap`House2`,
+tilemap`House3`,
+tilemap`Big House1`,
+tilemap`Big House Upper`,
+tilemap`Store`,
+tilemap`Store lvl2`,
+tilemap`Hospital`,
+tilemap`maze 1`,
+tilemap`maze 2`,
+tilemap`maze 3`,
+tilemap`Armory`,
+tilemap`House0`,
+tilemap`Store0`
+]
+tiles.setCurrentTilemap(maps[1])
+level = 1
+mySprite = sprites.create(assets.image`character`, SpriteKind.Player)
+mySprite.z = 1
+tiles.placeOnTile(mySprite, tiles.getTileLocation(4, 2))
+if (level == 1) {
+    controller.moveSprite(mySprite, 100, 100)
+    scene.cameraFollowSprite(mySprite)
+    mySprite.setScale(2, ScaleAnchor.Middle)
+}
+multilights.toggleLighting(true)
+multilights.addLightSource(
+mySprite,
+0,
+60,
+1
+)
+forever(function () {
+    if (level != 0) {
+        sprites.destroyAllSpritesOfKind(SpriteKind.npc)
+    }
+    if (level != 5) {
+        sprites.destroyAllSpritesOfKind(SpriteKind.npc1)
+    }
+})
+forever(function () {
+	
+})
