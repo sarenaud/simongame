cd9c1e69f051889fca3850e95438adfe1f31955a
sarenaud <32renaud.simon@gmail.com>
2025-03-31 12:06:33 -0400

    Update pxt.json, main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts, images.g.jres, images.g.ts

--- images.g.ts
+++ images.g.ts
@@ -3,23 +3,6 @@ namespace myImages {
 
     helpers._registerFactory("image", function(name: string) {
         switch(helpers.stringTrim(name)) {
-            case "image1":
-            case "cat_maybe":return img`
-. . . . . . . . . . . . . . 
-e e e . . . . e e e . . . . 
-c d d c . . c d d c . . . . 
-c b d d f f d d b c . . . . 
-c 3 b d d b d b 3 c . . . . 
-f b 3 d d d d 3 b f . . . . 
-e d d d d d d d d e . . . . 
-e d f d d d d f d e . b f b 
-f d d f d d f d d f . f d f 
-f b d d b b d d 2 b f f d f 
-. f 2 2 2 2 2 2 d b b d b f 
-. f d d d d d d d f f f f . 
-. . f d b d f d f . . . . . 
-. . . f f f f f f . . . . . 
-`;
             case "image3":
             case "mom":return img`
 . . . . . . . . . . . . . . . . 
@@ -186,6 +169,23 @@ d d d 3 4 f 4 3 3 4 f 4 3 d d d
 d d d 3 4 4 4 3 3 4 4 4 3 d d d 
 d d d 3 4 f 4 3 3 4 f 4 3 d d d 
 d d d 3 4 4 4 3 3 4 4 4 3 d d d 
+`;
+            case "image1":
+            case "cat_maybe":return img`
+. . . . . . . . . . . . . . 
+e e e . . . . e e e . . . . 
+c d d c . . c d d c . . . . 
+c b d d f f d d b c . . . . 
+c 3 b d d b d b 3 c . . . . 
+f b 3 d d d d 3 b f . . . . 
+e d d d d d d d d e . . . . 
+e d f d d d d f d e . b f b 
+f d d f d d f d d f . f d f 
+f b d d b b d d 2 b f f d f 
+. f 2 2 2 2 2 2 d b b d b f 
+. f d d d d d d d f f f f . 
+. . f d b d f d f . . . . . 
+. . . f f f f f f . . . . . 
 `;
         }
         return null;
